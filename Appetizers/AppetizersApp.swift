20ee2e8e57d4e0cfa364a4e189a3e3d0f29f9895
PrathameshChakote <pbc180798@gmail.com>
2023-12-18 16:02:54 -0800

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Prathamesh on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
